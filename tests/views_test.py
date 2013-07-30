310f3ca3284678e9ccb3c597179c6953e47d31da
Paul Ollivier <contact@paulollivier.fr>
2013-07-30 19:09:41 +0200

    Added some more tests, rewrote some views. Added travis-ci integration.

--- tests/views_test.py
+++ tests/views_test.py
@@ -1,6 +1,7 @@
 import os
 import unittest
-from wiki import app, db
+from wiki import app, db, models
+
 
 class WikiPageTest(unittest.TestCase):
     def setUp(self):
@@ -13,6 +14,8 @@ class WikiPageTest(unittest.TestCase):
         db.session.remove()
         db.drop_all()
 
+    # Base and core tests
+
     def test_redirects(self):
         r = self.app.get('/HomePage')
         self.assertEqual(r.status_code, 302)
@@ -23,8 +26,16 @@ class WikiPageTest(unittest.TestCase):
         self.assertIn("HomePage", r.data)
         self.assertNotIn("The page HomePage doesn't exist! Would you like to", r.data)
 
+    def test_help(self):
+        r = self.app.get('/WikiHelp/')
+        self.assertEqual(r.status_code, 200)
+        self.assertIn("WikiHelp", r.data)
+        self.assertNotIn("The page WikiHelp doesn't exist! Would you like to", r.data)
+
+    # Page url formatting tests
+
     def test_nonexistent_page(self):
-        r = self.app.get('/Blblblblbl/', follow_redirects=True)
+        r = self.app.get('/Blblblblbl/')
         self.assertEqual(r.status_code, 200)
         self.assertIn("The page Blblblblbl doesn't exist! Would you like to", r.data)
 
@@ -32,9 +43,64 @@ class WikiPageTest(unittest.TestCase):
         r = self.app.get("/not_correct/")
         self.assertEqual(r.status_code, 404)
 
+    # edit and save functionalities
+
     def test_edit_wikipage(self):
         r = self.app.get('/HomePage/edit')
         self.assertEqual(r.status_code, 200)
         self.assertIn("<form id=\"wikipageedit\"", r.data)
 
+    def test_edit_incorrect_url(self):
+        r = self.app.get('/invalid_url/edit')
+        self.assertEqual(r.status_code, 404)
+
+    def test_edit_post_wikipage(self):
+        r = self.app.post('/HomePage/edit', data={
+            "content": """# HomePage
+
+Hello"""
+        })
+        self.assertEqual(r.status_code, 200)
+        self.assertIn("""# HomePage
+
+Hello""", r.data)
+
     # TODO: test saves.
+
+    def test_save_new_wikipage(self):
+        PAGE_NAME = "HiMyNameIsPaul"
+        r = self.app.post('/%s/save' % PAGE_NAME, data={
+            "content": """# %s
+
+My name is Paul
+""" % PAGE_NAME
+        }, follow_redirects=True)
+        self.assertEqual(r.status_code, 200)
+        self.assertIn(PAGE_NAME, r.data)
+        self.assertIn("My name is Paul", r.data)
+
+    def test_save_existing_wikipage(self):
+        PAGE_NAME = "HomePage"
+        r = self.app.post('/%s/save' % PAGE_NAME, data={
+            "content": """# %s
+
+My name is Paul
+""" % PAGE_NAME
+        }, follow_redirects=True)
+        self.assertEqual(r.status_code, 200)
+        self.assertIn(PAGE_NAME, r.data)
+        self.assertIn("My name is Paul", r.data)
+
+    def test_get_on_save(self):
+        r = self.app.get('/HomePage/save')
+        self.assertEqual(r.status_code, 405)
+
+    def test_save_on_incorrect_page_title(self):
+        PAGE_NAME = "incorrect_url"
+        r = self.app.post('/%s/save' % PAGE_NAME, data={
+            "content": """# %s
+
+My name is Paul
+""" % PAGE_NAME
+        }, follow_redirects=True)
+        self.assertEqual(r.status_code, 404)
--- wiki/views.py
+++ wiki/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, redirect, request
+from flask import render_template, redirect, request, abort
 
 from wiki import app, models
 from wiki.models import ValidationError, get_page
@@ -8,9 +8,16 @@ from wiki.models import ValidationError, get_page
 def home():
     return redirect('/HomePage')
 
+def is_pagetitle_valid(pagetitle):
+    match = models.WikiPage.title_regex.search(pagetitle)
+    if(not match):
+        return False
+    return True
 
 @app.route('/<pagetitle>/edit', methods=['GET', 'POST'])
 def wikipageedit(pagetitle):
+    if(not is_pagetitle_valid(pagetitle)):
+        abort(404)
     data = {
             "pagetitle": pagetitle
             }
@@ -20,7 +27,7 @@ def wikipageedit(pagetitle):
 
     else:
         page = get_page(pagetitle)
-        if(page != []):
+        if(page != None):
             data["page"] = page
 
     return render_template('wikipage.edit.html', data=data)
@@ -28,27 +35,32 @@ def wikipageedit(pagetitle):
 
 @app.route('/<pagetitle>/save', methods=['POST'])
 def wikipagesave(pagetitle):
-    if(request.method == 'POST'):
-        try:
-            page = get_page(pagetitle)
-            if(page is not None):
-                page.content = request.form['content']
-            else:
-                page = models.WikiPage(request.form['content'])
-            page.save()
-            return redirect('/' + page.title)
-        except ValidationError as e:
-            # Here set field Error and make a POST request to /edit
-            return "ValidationError: " + e.message, 500
+    if(not is_pagetitle_valid(pagetitle)):
+        abort(404)
+    try:
+        page = get_page(pagetitle)
+        if(page is not None):
+            page.content = request.form['content']
+        else:
+            page = models.WikiPage(request.form['content'])
+        page.save()
+        return redirect('/' + page.title)
+    except ValidationError as e:
+        # Here set field Error and make a POST request to /edit
+        return "ValidationError: " + e.message, 500
 
 
 @app.route('/<pagetitle>')
 def wikipageredirect(pagetitle):
+    if(not is_pagetitle_valid(pagetitle)):
+        abort(404)
     return redirect('/%s/' % pagetitle)
 
 
 @app.route('/<pagetitle>/')
 def wikipageview(pagetitle):
+    if(not is_pagetitle_valid(pagetitle)):
+        abort(404)
     page = get_page(pagetitle)
     data = {
             "pagetitle": pagetitle

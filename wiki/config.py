44ba0459104aa17622ace19e98fbf731358ca30f
Paul Ollivier <paul.ollivier@ullink.com>
2013-08-02 15:21:05 +0200

    Models changes
    Added User model, added delete method

--- wiki/config.py
+++ wiki/config.py
@@ -10,8 +10,13 @@ MARKDOWN_EXTS = [
     'admonition'
 ]
 
-if "DATABASE_URL" in os.environ:
+if("DATABASE_URL" in os.environ):
     SQLALCHEMY_DATABASE_URI = os.environ["DATABASE_URL"]
 else:
     dbpath = os.path.join(basedir, 'wiki.db')
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + dbpath
+
+if("SECRET_KEY" in os.environ):
+    SECRET_KEY = os.environ["SECRET_KEY"]
+else:
+    SECRET_KEY = "super-secret-of-death"
--- wiki/models.py
+++ wiki/models.py
@@ -1,5 +1,7 @@
 import markdown
 import re
+import hashlib
+import datetime
 from wiki import db, app
 from sqlalchemy.exc import OperationalError
 
@@ -24,6 +26,7 @@ class WikiPage(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(256), index=True)
     content = db.Column(db.Text)
+    author_id = db.Column(db.Integer(), db.ForeignKey("user.id"))
     title_regex = re.compile(r'([A-Z][a-z]*)+')
 
     def __init__(self, content):
@@ -36,9 +39,7 @@ class WikiPage(db.Model):
     @classmethod
     def gen_title(a, content):
         if("\n" in content):
-            print("found \\n")
             first_line = content.split('\n')[0]
-            print(first_line)
         else:
             first_line = content
         if('\r' in content):
@@ -48,7 +49,8 @@ class WikiPage(db.Model):
         match = a.title_regex.match(first_line)
         if(match is None):
             raise ValidationError(
-                "Given title (%s) does not match '^([A-Z][a-z]*)+$' !" % first_line)
+                "Given title (%s) does not match \
+                '^([A-Z][a-z]*)+$' !" % first_line)
         return first_line
 
     def save(self):
@@ -59,11 +61,62 @@ class WikiPage(db.Model):
         db.session.add(self)
         db.session.commit()
 
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
     def html(self):
         html = markdown.markdown(
             self.content,
             extensions=app.config['MARKDOWN_EXTS'])
         return html
+
+
+def log_user(login=None, password=None):
+    search = User.query.filter(
+        username=login, password=User.gen_passwd(password)).first()
+    if(search):
+        return search
+    return None
+
+
+def create_user(login, email, password):
+    u = User()
+    u.username = login
+    u.email = email
+    u._gen_passwd(password)
+    u.created_on = datetime.datetime.utcnow()
+    return u
+
+
+def gen_passwd(orig_passwd):
+        return hashlib.sha512(
+            orig_passwd + app.config["SECRET_KEY"]
+        ).hexdigest()
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(32), index=True, unique=True)
+    password = db.Column(db.String(128), unique=False)
+    email = db.Column(db.String(256), index=True, unique=True)
+    created_on = db.Column(db.DateTime())
+    pages = db.relationship('WikiPage', backref='user', lazy='dynamic')
+
+    def __repr__(self):
+        return '<User:%s>' % self.username
+
+    def gen_passwd(self, orig_passwd):
+        self.password = gen_passwd(orig_passwd)
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+        self = None
 
 
 class ValidationError(Exception):
